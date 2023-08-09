980d96438cd9b374320fe1885c915b94f3cd1754
Pixmonster <mateoamariles2304@gmail.com>
2023-08-09 10:59:12 -0500

    Integración con mysql

--- Finance/settings.py
+++ Finance/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "Finance.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mibilletera',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- test1/models.py
+++ test1/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from datetime import date
 
+class Ingresos (models.Model):
+    cantidad_ingreso = models.IntegerField(blank=False, null= False)
+    fuente_ingreso = models.CharField(max_length=50, blank=True, null=True)
+    fecha_ingreso = models.DateField()
 
+    def __str__(self):
+        return self.fuente_ingreso
+
+class Ahorros (models.Model):
+    monto_ahorro = models.IntegerField(blank=False, null=False)
+    fecha_ini_ahorro = models.DateField()
+    fecha_fin_ahorro = models.DateField()
+    ingresos = models.ForeignKey(Ingresos, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.monto_ahorro
+
+class Gastos (models.Model):
+    monto_gasto = models.IntegerField(blank=False, null=False)
+
+    def __str__(self):
+        return self.monto_gasto
 
